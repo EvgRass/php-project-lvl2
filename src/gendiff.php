28b99081430073a5b2645ca34a9640fccceda225
EvgRass <johnrass@yandex.ru>
2022-06-29 12:22:47 +0300

    step 1 init docopt

---
+++ src/gendiff.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Gendiff;
+
+function gendiff()
+{
+    $doc = <<<DOC
+    Generate diff
+    Usage:
+      gendiff (-h|--help)
+      gendiff (-v|--version)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
+    Options:
+      -h --help                     Show this screen
+      -v --version                  Show version
+      --format <fmt>                Report format [default: stylish] 
+    DOC;
+
+    $args = \Docopt::handle($doc, array('vesion' => 'Rassolka 1.0'));
+
+    foreach ($args as $k => $v)    
+        echo $k . ': ' . json_encode($v) . PHP_EOL;
+}
