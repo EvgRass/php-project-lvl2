59f4fd59f46eaefc631f065edc2c15c1c2a12f0c
EvgRass <johnrass@yandex.ru>
2022-06-30 13:15:42 +0300

    step 3 Flat File Comparison (JSON)

--- src/gendiff.php
+++ src/gendiff.php
@@ -2,9 +2,81 @@
 
 namespace Gendiff;
 
-function gendiff($firstFile, $secondFile, $format)
+use function Functional\flatten;
+
+function gendiff(string $pathFirstFile, string $pathSecondFile, string $format)
 {
-    print_r($firstFile . ' ');
-    print_r($secondFile . ' ');
-    print_r($format . ' ');
+    $firstFileData = getFileData($pathFirstFile);
+    $secondFileData = getFileData($pathSecondFile);
+
+    $tree = getDiffTree($firstFileData, $secondFileData);
+
+    return format($format, $tree);
+}
+
+function getFileData(string $pathFile): array
+{
+    return json_decode(file_get_contents($pathFile), 1);
+}
+
+function getDiffTree(array $firstFileData, array $secondFileData): array
+{
+    $uniqKeys = array_unique(array_merge(array_keys($firstFileData), array_keys($secondFileData)));
+    sort($uniqKeys);
+
+    return array_map(function ($key) use ($firstFileData, $secondFileData) {
+        if (!array_key_exists($key, $secondFileData)) {
+            return [
+                'name' => $key,
+                'type' => 'removed',
+                'value' => $firstFileData[$key]
+            ];
+        }
+        if (!array_key_exists($key, $firstFileData)) {
+            return [
+                'name' => $key,
+                'type' => 'added',
+                'value' => $secondFileData[$key]
+            ];
+        }
+        if ($firstFileData[$key] !== $secondFileData[$key]) {
+            return [
+                'name' => $key,
+                'type' => 'changed',
+                'valueFirst' => $firstFileData[$key],
+                'valueSecond' => $secondFileData[$key]
+            ];
+        }
+        return [
+            'name' => $key,
+            'type' => 'unchanged',
+            'value' => $firstFileData[$key]
+        ];
+    }, $uniqKeys);
+}
+
+function format(string $format, array $tree)
+{
+    if ($format === 'stylish') {
+        $acc = [];
+        foreach ($tree as $k => $item) {
+            switch ($item['type']) {
+                case 'removed':
+                    $acc["- " . $item['name']] = $item['value'];
+                    break;
+                case 'added':
+                    $acc["+ " . $item['name']] = $item['value'];
+                    break;
+                case 'changed':
+                    $acc["- " . $item['name']] = $item['valueFirst']; 
+                    $acc["+ " . $item['name']] = $item['valueSecond'];
+                    break;
+                case 'unchanged':
+                    $acc[$item['name']] = $item['value'];
+                    break;
+            }
+        }
+        return json_encode($acc);
+    }
+    
 }
